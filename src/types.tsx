85db06e336556447233b4c17445246062bbff646
Noel Lopez <nolo@Noels-MacBook-Pro.local>
2026-03-27 14:55:54 -0700

    minor build fix , plus type update

--- src/types.tsx
+++ src/types.tsx
@@ -1,24 +1,24 @@
-export interface BookPage{
+export type BookPage = {
     count:number;
     next:string;
     previous:string;
     results:Book[]
 }
 
-export interface Book{
+export type Book = {
     id:number;
     title:string;
     authors:Author[];
     formats:Format;
 }
 
-export interface Author{
+export type  Author = {
     name:string;
     birth_year:number;
     death_year:number;
 }
 
-export interface Format{
+export type Format = {
     "text/html":string;
     "image/jpeg":string;
 }
